cc2ed57f076171c1655369e9d902e5c42190141c
dovahkiin1993 <ashleysnape9@gmail.com>
2023-02-15 21:22:04 +0000

    updated javascript and added coments

--- script.js
+++ script.js
@@ -6,54 +6,64 @@ var numbers = [0,1,2,3,4,5,6,7,8,9];
 var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
 var min = [8];
 var max = [128];
-var finalPass = ""
-var finalChoices = []
 
+// This function is asking the user what they would like in their password.
+// It is also capturing what the user is inputting and generates a password accordingly
 
 function generatePassword(){
-  let passwordlink = prompt ("please enter password length characters between 8-128");
-  console.log(passwordlink)
+  var finalChoices = []
+  let passwordlink = prompt ("Please enter the password length you require between 8-128 characters");
+  // console.log(passwordlink)
   if(passwordlink > 8 && passwordlink < 128){
-    console.log("sucsess")
-    let capitalConfirm = confirm ("Do you want capital letters in your password?")
-    console.log(capitalConfirm)
+    // console.log("sucsess")
+    let capitalConfirm = confirm ("Do you want capital letters in your password? choose ok for yes or cancel for no")
+    // console.log(capitalConfirm)
     if(capitalConfirm == true ){
-      console.log("caps added")
+      // console.log("caps added")
       finalChoices = finalChoices.concat(uppercase)
     }
   
-    let lowerConfirm = confirm("Do you want lowercase letters in your password?")
+    let lowerConfirm = confirm("Do you want lowercase letters in your password? choose ok for yes or cancel for no")
     if(lowerConfirm == true){
-      console.log("lower case added")
+      // console.log("lower case added")
       finalChoices = finalChoices.concat(lowercase)
 
     }
 
-    let numConfirm = confirm("Do you want a number in your password?")
+    let numConfirm = confirm("Do you want any numbers in your password? choose ok for yes or cancel for no")
     if(numConfirm == true){
-      console.log("nums added")
+      // console.log("nums added")
       finalChoices = finalChoices.concat(numbers)
     }
 
-    let specConfirm = confirm("Do you want special characters in you password?")
+    let specConfirm = confirm("Do you want any special characters in your password? choose ok for yes or cancel for no")
     if(specConfirm == true){
-      console.log(" special characters added")
+      // console.log(" special characters added")
       finalChoices = finalChoices.concat(special)
 
     }
 
-    console.log(finalChoices.length)
-
-    for(i = 0; i < finalChoices.length; i++){
+    // console.log(passwordlink.length)
+    var passlength = parseInt(passwordlink)
+    var finalPass = ""
+    for(i = 0; i < passlength; i++){
+      var randomIndex = Math.floor(Math.random() * finalChoices.length)
+      var randomChar = finalChoices[randomIndex]
+      finalPass += randomChar
+// grab a random index based on finalchoices.length
+// grab a character from final choices using random index
+// add characters to the final pass
 
     }
+
+     
 
     // find a way to loop final choices 
     // find a way to get random letters from final choices
     // adding the random letters into finalpass var
 
     return finalPass
-  } else{alert("Please choose the correct length")}
+  } else{alert("Please choose the correct length of characters")}
 
 } 
 
@@ -63,6 +73,7 @@ function generatePassword(){
 
 // Write password to the #password input
 function writePassword() {
+
   var password = (generatePassword)("#password");
   var passwordText = document.querySelector("#password");
 
